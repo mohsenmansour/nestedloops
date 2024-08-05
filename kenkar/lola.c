99a95d1bc10aa0b76ec30f8d0548fcacc479cda7
aminamohsen <abdalmohsenmansour1@gmail.com>
2024-08-05 19:06:25 +0300

    lol

---
+++ kenkar/lola.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main()
+{
+for(int i=65; i<=80; i++)
+{
+printf("%c ", i);
+}
+printf("\n");
+}
---
+++ kenkar/lolb.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main()
+{
+for(char c='a'; c<='z'; c++)
+{
+printf("%c ", c);
+}
+printf("\n");
+}
