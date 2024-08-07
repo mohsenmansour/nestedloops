d133738f1b3b9c8b2856d4e2a41138f63b39921a
aminamohsen <abdalmohsenmansour1@gmail.com>
2024-08-07 17:46:17 +0300

    lol

---
+++ kenkar/firstdigit2.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+    int n, first;
+printf("Enter any number: ");
+    scanf("%d", &n);
+
+    first = n;
+while(first >= 10)
+    {
+        first = first / 10;
+printf("First digit = %d\n", first);
+}
+return 0;
+}
