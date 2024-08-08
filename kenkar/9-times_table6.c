3c9422cf647c83f98b3c70c49c3db646255ac0ba
aminamohsen <abdalmohsenmansour1@gmail.com>
2024-08-08 14:54:16 +0300

    lol

---
+++ kenkar/9-times_table6.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main() {
+  {
+        int i, j, k;
+
+        for (i = 0; i < 10; i++)
+        {
+                for (j = 0; j < 10; j++)
+                {
+                        k ='0';
+if (j == 0)
+                        {
+putchar(k);
+}
+  }
+putchar('\n');
+        }
+}
+}
