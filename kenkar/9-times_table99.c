2aa2f108023b53dc8006a9bd296135dbebe53e19
aminamohsen <abdalmohsenmansour1@gmail.com>
2024-08-09 01:56:40 +0300

    lol

---
+++ kenkar/9-times_table99.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main() {
+  {
+        int i, j, k;
+
+        for (i = 0; i < 10; i++)
+        {
+                for (j = 0; j < 10; j++)
+                {
+                        k = j * i;
+                        if (j == 0)
+                        {
+printf("%d",k);
+}
+}
+printf("\n");
+}
+}
+}
