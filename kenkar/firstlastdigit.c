362308aaafccdeff516db35d78886bb2c34ef45b
aminamohsen <abdalmohsenmansour1@gmail.com>
2024-08-10 01:51:27 +0300

    lol

---
+++ kenkar/firstlastdigit.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int main()
+{
+int number, first_digit, last_digit;
+// Input from the user
+    printf("Enter a number: ");
+    scanf("%d", &number);
+// Finding the last digit
+    last_digit = number % 10;
+
+    // Finding the first digit
+    while (number >= 10) {
+        number /= 10;
+    }
+    first_digit = number;
+
+    // Displaying the first and last digits
+    printf("First digit: %d\n", first_digit);
+    printf("Last digit: %d", last_digit);
+
+    return 0;
+}
---
+++ kenkar/firstlastdigit3.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main()
+{
+int number, first_digit, last_digit;
+printf("Enter a number: ");
+    scanf("%d", &number);
+last_digit = number % 10;
+while (number >= 10)
+{
+number=number/10;
+first_digit=number;
+}
+printf("First digit: %d\n", first_digit);
+    printf("Last digit: %d\n", last_digit);
+}
+return 0;
+}
