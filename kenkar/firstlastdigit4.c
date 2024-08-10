b72d3caf561d776e9b30e2024ec535e2730f9196
aminamohsen <abdalmohsenmansour1@gmail.com>
2024-08-10 14:59:11 +0300

    lol

--- kenkar/firstlastdigit4.c
+++ kenkar/firstlastdigit4.c
@@ -4,11 +4,11 @@ int main()
 int number, first_digit, last_digit;
 printf("Enter a number: ");
     scanf("%d", &number);
-last_digit = number % 10;
 while (number >= 10)
 {
 number=number/10;
 first_digit=number;
+last_digit = number % 10;
 printf("First digit: %d\n", first_digit);
     printf("Last digit: %d\n", last_digit);
 }
