75e6b27e3f35054588a0b0b703d28b682267f0b6
aminamohsen <abdalmohsenmansour1@gmail.com>
2024-08-12 00:46:29 +0300

    lol

---
+++ kenkar/proto3.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+void add()
+{
+printf("hello\n");
+}
+int main()
+{
+add();
+}
---
+++ kenkar/proto4.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+void add()
+{
+printf("hello\n");
+}
+void fun1()
+{
+printf("hala\n");
+}
+int main()
+{
+add();
+fun1();
+}
