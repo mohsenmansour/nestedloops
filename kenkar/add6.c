921b42e39eb1759c6d4a2688236bca266fb7ebcc
aminamohsen <abdalmohsenmansour1@gmail.com>
2024-08-12 01:36:14 +0300

    lol

---
+++ kenkar/add6.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int add(int a, int b)
+{
+return a + b;
+}
+int main()
+{
+add(89, 9);
+printf("%d\n",add(89, 9));
+}
---
+++ kenkar/proto6.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int lolo(int num)
+{
+printf("%d\n", num);
+}
+int main()
+{
+lolo(98);
+}
