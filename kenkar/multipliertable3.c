8ff8ea42f4aacb74cc1de35d5226b561bd7d47cc
aminamohsen <abdalmohsenmansour1@gmail.com>
2024-08-13 23:21:35 +0300

    lol

---
+++ kenkar/multipliertable3.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+int main() {
+    int j, i, n;
+printf("Input upto the table number starting from 1 : ");
+scanf("%d", &n);
+printf("Multiplication table from 1 to %d \n", n);
+for (i = 1; i <= 10; i++)
