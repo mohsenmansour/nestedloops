39809416cb327d4ed07b9d44c5c94751c4b10765
aminamohsen <abdalmohsenmansour1@gmail.com>
2024-08-14 15:39:40 +0300

    lol

---
+++ kenkar/multipliertable8.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+int main() {
+    int j, i, n;
+printf("Input upto the table number starting from 1 : ");
+scanf("%d", &n);
+printf("Multiplication table from 1 to %d \n", n);
+for (i = 1; i <= 10; i++)
+{
+for (j = 1; j <= n; j++)
+{
+printf("%dx%d = %d| ", j, i, i * j);
+if(i*j<=9)
+{
+printf("%dx%d  =  %d|", j, i, i * j);
+}
+else if(i*j>9)
+{
+printf("%dx%d = %d|  ", j, i, i * j);
+}
+printf("\n");
+}
+
+    return 0;
+}
+}
