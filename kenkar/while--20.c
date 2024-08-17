42505aedac0a31aa9c7afb8874087d758bb34200
aminamohsen <abdalmohsenmansour1@gmail.com>
2024-08-18 00:23:42 +0300

    lol

---
+++ kenkar/while--20.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main ( )
+
+{
+
+int i=0;
+while(i<10)
+{
+i++;
+printf("%d",i/2);
+printf("%d\n",i);
+}
+printf("\n");
+printf("%d",i);
+printf("\n");
+}
---
+++ kenkar/while--22.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main ( )
+
+{
+
+int i=0;
+while(i<10)
+{
+printf("%d\n",i);
+printf("%d",i/2);
+i++;
+}
+printf("\n");
+printf("%d",i);
+printf("\n");
+}
