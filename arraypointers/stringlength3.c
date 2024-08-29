abdf124ca085eae7ee60265d331b0947f409929c
aminamohsen <abdalmohsenmansour1@gmail.com>
2024-08-29 16:05:53 +0300

    lol

---
+++ arraypointers/stringlength3.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+{
+  char s[1000];
+  int c=0;
+ printf("Input a string\n");
+ scanf("%s",s);
+
+  while (s[c] != '\0')
+    c++;
+
+  printf("Length of the string: %d\n", c);
+
+  return 0;
+}
