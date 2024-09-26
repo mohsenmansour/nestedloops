799c1248c2534d5b2c22b718d39c7703694f67bd
aminamohsen <abdalmohsenmansour1@gmail.com>
2024-09-26 18:27:04 +0300

    ahmedengagment

---
+++ morepointers/strcat5.c
@@ -0,0 +1,26 @@
+// C Program to concatenate two strings manually using loops
+#include <stdio.h>
+void concatStr(char *dest, const char *src) {
+    int length, j;
+    // Find the length of the dest string
+    for (length = 0; dest[length] != '\0'; length++);
+
+    // Append the src string to the dest string
+    for (j = 0; src[j] != '\0'; j++, length++)
+    {
+        dest[length] = src[j];
+    }
+    // Null-terminate the concatenated dest string
+    dest[length] = '\0';
+}
+
+int main() {
+    char dest[100] = "Geeksfor";
+    char src[100] = "Geeks";
+
+    // Call the concatStr function
+    concatStr(dest, src);
+
+    printf("Concatenated String: %s\n", dest);
+    return 0;
+}
---
+++ morepointers/strcat7.c
@@ -0,0 +1,32 @@
+// C Program to concatenate two strings manually using loops
+#include <stdio.h>
+void concatStr(char *dest, const char *src)
+{
+    int length, j;
+    // Find the length of the dest string
+    for (length = 0; dest[length] != '\0'; length++);
+    printf("%d\n",length);
+
+    // Append the src string to the dest string
+    for (j = 0; src[j] != '\0'; j++, length++);
+    {
+        dest[length]= src[j] ;
+        printf("%d\n",length);
+    }
+    // Null-terminate the concatenated dest string
+  
+    dest[length] = '\0';
+}
+
+int main()
+{
+   
+    char dest[100] = "Geeksfor";
+    char src[100] = "Geeks";
+
+    // Call the concatStr function
+    concatStr(dest, src);
+
+    printf("Concatenated String: %s\n", dest);
+    return 0;
+}
