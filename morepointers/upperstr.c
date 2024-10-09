74f266b026ac7a5c32f3351d091602b7c533b1d1
aminamohsen <abdalmohsenmansour1@gmail.com>
2024-10-10 00:06:07 +0300

    ahmedengagment

---
+++ morepointers/upperstr.c
@@ -0,0 +1,26 @@
+// C Program to reverse an array by swapping elements
+
+#include <stdio.h>
+
+char *string_toupper(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] >= 'a' && s[i] <= 'z')
+			s[i] = s[i] - 32;
+	}
+
+	return (s);
+}
+int main(void)
+{
+    char str[] = "Look up!\n";
+    char *ptr;
+
+    ptr = string_toupper(str);
+    printf("%s", ptr);
+    printf("%s", str);
+    return (0);
+}
