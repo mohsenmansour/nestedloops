86aa70efad270d2ad7a653acc63de081a0940cde
aminamohsen <abdalmohsenmansour1@gmail.com>
2024-10-18 01:15:57 +0300

    ahmedengagment

--- morepointers/upperstr6.c
+++ morepointers/upperstr6.c
@@ -1,18 +1,16 @@
 #include <stdio.h>
 char *cap_string(char *s)
 {
-	int i, j;
+        int i, j;
 for (i = 0; s[i] != '\0'; i++)
-	{
+        {
 if (s[i + 1] >= 'a' && s[i + 1] <= 'z')
-				{
-					s[i + 1] -= 32;
-				}
-
-
-	return (s);
+                                {
+                                        s[i + 1] -= 32;
+                                }
 }
-
+return (s);
+}
 int main(void)
 {
 char str[] = "Expect the best. Prepare for the worst. Capitalize on what comes.\nhello world! hello-world 0123456hello world\thello world.hello world\n";
