9c454af69b22c4fa02725d268cf07ffa63435ab6
aminamohsen <abdalmohsenmansour1@gmail.com>
2024-10-18 17:55:55 +0300

    ahmedengagment

---
+++ morepointers/upperstr9.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+int main(void)
+{
+           int i, j;
+char str[] = "Expect the best. Prepare for the worst. Capitalize on what comes.\nhello world! hello-world 0123456hello world\thello world.hello world\n";
+char fig[13] = {' ', '\t', '\n', ',', ';', '.',
+		'!', '?', '"', '(', ')', '{', '}'};
+char *ptr=&str;
+
+for (i = 0; str[i] != '\0'; i++)
+{
+if (str[i] >= 'a' )
+                                {
+                                        str[i ] -= 32;
+                                }
+    for (j = 0; j < 13; j++)
+		{
+			if (str[i] == fig[j])
+			{
+				if (str[i + 1] >= 'a' && str[i + 1] <= 'z')
+				{
+					str[i + 1] -= 32;
+}
+}
+}
+}
+printf("%s", str);
+printf("%s", ptr);
+}
+
+
+
+
+
+
