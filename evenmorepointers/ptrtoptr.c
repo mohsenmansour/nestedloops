37442f287471f08d977c8a6afa182c3b9914af08
aminamohsen <abdalmohsenmansour1@gmail.com>
2024-10-27 16:29:39 +0300

    ahmedengagment

---
+++ evenmorepointers/ptrtoptr.c
@@ -0,0 +1,21 @@
+// C program to demonstrate pointer to pointer
+#include <stdio.h>
+int main()
+{
+    int var = 789;
+    // pointer for var
+    int* ptr1;
+    // double pointer for ptr1
+    int** ptr2;
+    // storing address of var in ptr1
+    ptr1 = &var;
+    // Storing address of ptr1 in ptr2
+    ptr2 = &ptr1;
+    // Displaying value of var using
+    // both single and double pointers
+    printf("Value of var = %d\n", var);
+    printf("Value of var using single pointer = %d\n", *ptr1);
+    printf("Value of var using double pointer = %d\n", **ptr2);
+
+    return 0;
+}
